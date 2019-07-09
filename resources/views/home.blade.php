0aa7f7b38122bd56ec76b758a4acd58a7d313545
geert timmermans <geert.timmermans80@gmail.com>
2019-07-09 16:20:59 +0200

    .

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-12 col-md-5 offset-md-1 d-flex flex-column justify-content-center align-items-center order-1 order-md-0">
+            <div class="col-12 col-md-5 offset-md-1 d-flex flex-column justify-content-center align-items-end align-items-md-center order-1 order-md-0">
                 <a href="{{ route('hikes') }}" class="btn btn-info w-50 my-3">List Hikes</a>
                 <a href="{{ route('createHike') }}" class="btn btn-info w-50 my-3">Add Hike</a>
                 <a href="{{ route('editProfile') }}" class="btn btn-info w-50 my-3">Edit Profile</a>
