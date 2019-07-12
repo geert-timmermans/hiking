16041e300a307903696b031cb0b556aa22f0213d
geert timmermans <geert.timmermans80@gmail.com>
2019-07-12 10:42:28 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/hikes', 'HikeController@index')->name('hikes');
 
